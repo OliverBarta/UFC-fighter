59e3e36c6b3ee6e7dc9f66c65cd7543b49d473ce
OliverBarta <barta3738@gmail.com>
2025-10-22 20:09:26 -0400

    split the files and furthered develp

---
+++ scriptbackend.js
@@ -0,0 +1,21 @@
+document.getElementById("fightersearch").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        searchFighter();
+    }
+});
+
+function searchFighter() {
+    const fighterName = document.getElementById("fightersearch").value.trim();
+    const resultDiv = document.getElementById("result");
+
+    if (fighterName === "") {
+        resultDiv.style.display = "block";
+        resultDiv.innerHTML = "⚠️ Please enter a fighter name.";
+        return;
+    }
+
+
+
+    resultDiv.style.display = "block";
+    resultDiv.innerHTML = fighterName;
+}
